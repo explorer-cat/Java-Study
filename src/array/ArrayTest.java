736ba9d97c3c9fa3d38720b590c2a1b10d671eb8
explorer-cat <sqlstyle@naver.com>
2023-02-01 18:18:22 +0900

    ArrayList studey

---
+++ src/array/ArrayTest.java
@@ -0,0 +1,15 @@
+package array;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+//        int[] numbers = new int[] {0,1,2};
+
+        int[] numbers = new int[]{0, 1, 2};
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
